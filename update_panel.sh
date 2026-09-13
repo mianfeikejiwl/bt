735f0d9db7354369146ba0e2a3dad5e652fa64b6
huolangwangluo <huolangwangluo@outlook.com>
2026-09-13 12:11:00 +0800

    宝塔

--- update_panel.sh
+++ update_panel.sh
@@ -70,7 +70,7 @@ select_node(){
     . $public_file
 
     download_Url=$NODE_URL
-		downloads_Url=https://raw.githubusercontent.com/mianfeikejiwl/bt/refs/heads/main
+		downloads_Url=https://gh-proxy.org/https://raw.githubusercontent.com/mianfeikejiwl/bt/refs/heads/main
 }
 
 get_version(){
